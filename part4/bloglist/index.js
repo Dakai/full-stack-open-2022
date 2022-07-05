6f7253393038b85cc82e31edfa9408ed5ed1bb44
Dakai <oudakai@gmail.com>
2022-07-05 23:16:29 +0800

    Part4 exercise start

---
+++ part4/bloglist/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
