3497d4afd52789e41de6da611549a0a532b73406
Dakai <oudakai@gmail.com>
2022-07-22 23:34:36 +0800

    Solution for exercise 4.18

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
-const User = require('..models/user')
+const User = require('../models/user')
 
 loginRouter.post('/', async (req, res) => {
   const { username, password } = req.body
@@ -20,6 +20,7 @@ loginRouter.post('/', async (req, res) => {
     username: user.username,
     id: user._id,
   }
+
   const token = jwt.sign(userForToken, process.env.SECRET)
 
   res.status(200).send({ token, username: user.username, name: user.name })
